58316c266b3cba84aba833187c5d4d1086028914
teojiesern <teojiesern42@gmail.com>
2024-03-24 21:35:46 +0800

    conf/css reset

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './app/platform/router';
+import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
