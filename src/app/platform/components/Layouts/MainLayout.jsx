7b8ec6feb3deccfb100ab67b021b24dfcd85c034
teojiesern <teojiesern42@gmail.com>
2024-04-02 23:28:34 +0800

    feat(layout): add layout

--- src/app/platform/components/Layouts/MainLayout.jsx
+++ src/app/platform/components/Layouts/MainLayout.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LayoutContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0rem 5rem 2rem 5rem;
+`;
 
 export default function MainLayout() {
     return (
-        <div>
+        <LayoutContainer>
             Main Layout
             <Outlet />
-        </div>
+            Footer
+        </LayoutContainer>
     );
 }
