278d09455a74ab9c9928933d8a0adb97e74235fa
teojiesern <teojiesern42@gmail.com>
2024-04-06 22:04:55 +0800

    feat(main): consume logic

--- src/app/modules/main/view/MainScreen.jsx
+++ src/app/modules/main/view/MainScreen.jsx
@@ -1,20 +1,39 @@
 import styled from 'styled-components';
 import Footer from '../../../platform/components/Footer/Footer';
 import Header from '../../../platform/components/Header/Header';
+import homeBanner from '../data/homeBanner';
+import EmblaCarousel from './component/Embla/EmblaCarousel';
 
 const Container = styled.div`
     display: grid;
     grid-template-rows: auto 1fr auto;
-    padding: 2rem 5rem;
+    padding: 2rem 0;
     min-height: calc(100vh - 4rem);
+`;
+
+const PaddingHorizontal = styled.div`
+    padding: 0 5rem;
+`;
+
+const ContentContainer = styled.div`
+    margin: 2rem 0;
+    display: flex;
 `;
 
 export default function MainScreen() {
     return (
         <Container>
-            <Header />
-            <h1>Main screen</h1>
-            <Footer />
+            <PaddingHorizontal>
+                <Header />
+            </PaddingHorizontal>
+
+            <ContentContainer>
+                <EmblaCarousel banners={homeBanner} />
+            </ContentContainer>
+
+            <PaddingHorizontal>
+                <Footer />
+            </PaddingHorizontal>
         </Container>
     );
 }
