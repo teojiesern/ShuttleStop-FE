4d4a92e083f91c3995698fdc7c0446a60e67b987
teojiesern <teojiesern42@gmail.com>
2024-04-15 23:03:21 +0800

    feat(sellerCenter): my products UI

---
+++ src/app/platform/constants/PlatformConstants.js
@@ -0,0 +1,18 @@
+export const ProductCategories = Object.freeze({
+    RACQUETS: 'Racquets',
+    FOOTWEARS: 'Footwears',
+    APPARELS: 'Apparels',
+    BAGS: 'Bags',
+    SHUTTLECOCKS: 'Shuttlecocks',
+    ACCESSORIES: 'Accessories',
+});
+
+export const ProductBrands = Object.freeze({
+    YONEX: 'Yonex',
+    LI_NING: 'Li-Ning',
+    VICTOR: 'Victor',
+    ASICS: 'Asics',
+    APACS: 'Apacs',
+    FZ_FORZA: 'FZ Forza',
+    KAWASAKI: 'Kawasaki',
+});
