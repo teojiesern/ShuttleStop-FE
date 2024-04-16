20370b0161bc0d3817c088957615c3d2f39872b2
lichee03 <lichee03@gmail.com>
2024-04-16 19:53:07 +0800

    adding header for competition and coach and promote competition form

--- src/app/modules/marketing/navigation/MarketingScreens.jsx
+++ src/app/modules/marketing/navigation/MarketingScreens.jsx
@@ -1,15 +1,26 @@
 import { Route } from 'react-router-dom';
 import MarketingTestScreen from '../view/MarketingTestScreen';
 import MarketingTestScreenTwo from '../view/MarketingTestScreenTwo';
+import SeniorCompetitionScreen from '../view/SeniorCompetitionScreen';
+import CompetitionScreen from './CompetitionScreen';
 
 const MarketingScreens = (
     <>
         <Route
-            path="marketing/competitions"
-            element={<MarketingTestScreen />}
-        />
+            path="/marketing/competitions"
+            element={<CompetitionScreen />}
+        >
+            <Route
+                index
+                element={<MarketingTestScreen />}
+            />
+            <Route
+                path="senior"
+                element={<SeniorCompetitionScreen />}
+            />
+        </Route>
         <Route
-            path="marketing/coaches"
+            path="/marketing/coaches"
             element={<MarketingTestScreenTwo />}
         />
     </>
