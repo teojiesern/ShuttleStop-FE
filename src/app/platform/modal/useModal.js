6955ded1d50d0e4a0840aa17bb41e9af8c6ac3b1
teojiesern <teojiesern42@gmail.com>
2024-04-20 12:15:55 +0800

    feat(sellerCenter): modal size

--- src/app/platform/modal/useModal.js
+++ src/app/platform/modal/useModal.js
@@ -2,8 +2,8 @@ import { useCallback } from 'react';
 import ModalManager from './internal/ModalManager';
 
 const useModal = () => {
-    const showModal = useCallback(({ modal, disableBackdropDismiss }) => {
-        ModalManager.showModal({ modal, disableBackdropDismiss });
+    const showModal = useCallback(({ modal, disableBackdropDismiss, cmaxWidth }) => {
+        ModalManager.showModal({ modal, disableBackdropDismiss, cmaxWidth });
     }, []);
 
     const hideModal = useCallback(() => ModalManager.hideModal(), []);
