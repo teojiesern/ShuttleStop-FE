d282c69ec48aad300980f6f66f7e815d490dcf2d
teojiesern <teojiesern42@gmail.com>
2024-04-20 18:44:19 +0800

    feat(sellerCenter): edit button close button

--- src/app/modules/sellerCenter/view/modal/SCEditProductsModal.jsx
+++ src/app/modules/sellerCenter/view/modal/SCEditProductsModal.jsx
@@ -1,3 +1,5 @@
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import { IconButton } from '@mui/material';
 import styled from 'styled-components';
 import SCAddNewProductsScreen from '../screens/SCAddNewProductsScreen';
 
@@ -6,10 +8,19 @@ const CenteredDiv = styled.div`
     width: 90%;
 `;
 
-export default function SCEditProductsModal(props) {
+export default function SCEditProductsModal({ hideModal, ...props }) {
     return (
         <CenteredDiv>
-            <SCAddNewProductsScreen {...props} />
+            <IconButton
+                onClick={hideModal}
+                style={{ position: 'absolute', top: '2rem', right: '2rem' }}
+            >
+                <CloseOutlinedIcon />
+            </IconButton>
+            <SCAddNewProductsScreen
+                {...props}
+                hideModal={hideModal}
+            />
         </CenteredDiv>
     );
 }
