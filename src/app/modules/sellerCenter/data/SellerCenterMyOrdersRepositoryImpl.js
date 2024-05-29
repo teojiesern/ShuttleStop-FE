0a90a8c572a7deacecb5bb241f7f524e5e7ec585
teojiesern <teojiesern42@gmail.com>
2024-05-29 21:57:56 +0800

    update my orders repository layer

--- src/app/modules/sellerCenter/data/SellerCenterMyOrdersRepositoryImpl.js
+++ src/app/modules/sellerCenter/data/SellerCenterMyOrdersRepositoryImpl.js
@@ -4,27 +4,28 @@ export default class SellerCenterMyOrdersRepositoryImpl {
     #BASE_URL = '/seller-service';
 
     #ROUTES = {
-        // TODO: Update with actual route when BE is ready
-        GET_TO_SHIP_ORDERS: `${this.#BASE_URL}/...`,
+        TO_SHIP_ORDERS: (shopId) => `${this.#BASE_URL}/orders/to-ship/${shopId}`,
+        SHIPPING_ORDERS: (shopId) => `${this.#BASE_URL}/orders/shipping/${shopId}`,
+        COMPLETED_ORDERS: (shopId) => `${this.#BASE_URL}/orders/completed/${shopId}`,
     };
 
-    getToShipOrders = async () => {
-        const { status, data } = await Network.getInstance().get(this.#ROUTES.GET_TO_SHIP_ORDERS);
+    getToShipOrders = async (payload) => {
+        const { status, data } = await Network.getInstance().get(this.#ROUTES.TO_SHIP_ORDERS(payload.shopId));
+        return { status, data };
+    };
+
+    getShippingOrders = async (payload) => {
+        const { status, data } = await Network.getInstance().get(this.#ROUTES.SHIPPING_ORDERS(payload.shopId));
+        return { status, data };
+    };
+
+    getDeliveredOrders = async (payload) => {
+        const { status, data } = await Network.getInstance().get(this.#ROUTES.COMPLETED_ORDERS(payload.shopId));
         return { status, data };
     };
 
     shipOrders = async (orderIds) => {
         const { status, data } = await Network.getInstance().post(this.#ROUTES.GET_TO_SHIP_ORDERS, { orderIds });
-        return { status, data };
-    };
-
-    getShippingOrders = async () => {
-        const { status, data } = await Network.getInstance().get(this.#ROUTES.GET_TO_SHIP_ORDERS);
-        return { status, data };
-    };
-
-    getDeliveredOrders = async () => {
-        const { status, data } = await Network.getInstance().get(this.#ROUTES.GET_TO_SHIP_ORDERS);
         return { status, data };
     };
 }
